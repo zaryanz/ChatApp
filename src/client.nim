987d2f0c65f7e29e7453bcc18f50caf6653896da
Aryan Singh <aryanx404@gmail.com>
2023-06-18 21:27:45 +0530

    feat: initial project setup

---
+++ src/client.nim
@@ -0,0 +1,11 @@
+import os, threadpool
+
+echo("Chat application has started")
+if paramCount() == 0:
+    quit("Please specify the server address, e.g. ./client localhost")
+
+let serverAddr = paramStr(1)
+echo("Connecting to ", serverAddr)
+while true:
+    let message = spawn stdin.readLine()
+    echo("Sending \"", ^message, "\"")
